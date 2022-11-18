eb22ecf2856c93f58bdeee44056881a9f37462b9
larguesa <rlarguesa@gmail.com>
2022-11-18 16:18:19 -0300

    Excetion handling on HttpClient post and get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, VERSION } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 interface Auth {
   token: string;
@@ -11,43 +12,59 @@ interface Auth {
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  site = "https://glacial-scrubland-11809.herokuapp.com";
+export class AppComponent {
+  loading = false;
+  error = null;
+
+  site = 'https://glacial-scrubland-11809.herokuapp.com';
   name = 'Spring Boot sample';
 
-  login = "ricardo.larguesa@fatec.sp.gov.br";
-  password = "";
+  login = 'ricardo.larguesa@fatec.sp.gov.br';
+  password = '';
 
   auth = null;
 
   list = null;
-  
-  constructor(private http: HttpClient) { }
 
-  postLogin(){
-    this.http.post<Auth>(
-      this.site+'/login', 
-      { login: this.login, senha: this.password}
-    ).subscribe(data => {
+  constructor(private http: HttpClient) {}
+
+  postLogin() {
+    this.error = null;
+    this.loading = true;
+    let url = this.site + '/login';
+    let parameters = { login: this.login, senha: this.password };
+    this.http.post<Auth>(url, parameters).subscribe({
+      next: (data) => {
         this.auth = data;
-      }
-    );
+        this.loading = false;
+      },
+      error: (error) => {
+        this.error = error;
+        this.loading = false;
+      },
+    });
   }
 
-  postLogout(){
+  postLogout() {
     this.auth = null;
   }
 
-  getList(){
-    this.http.get<any>(
-      this.site+'/pessoas_fisicas', 
-      {headers: {'Authorization':'Bearer '+this.auth.token}}
-    ).subscribe(data => {
+  getList() {
+    this.loading = true;
+    this.error = null;
+    let url = this.site + '/pessoas_fisicas';
+    let parameters = { headers: { Authorization: 'Bearer ' + this.auth.token }};
+    this.http.get<any>(url, parameters).subscribe({
+      next: (data) => {
         this.list = data;
-      }
-    );
+        this.loading = false;
+      },
+      error: (error) => {
+        this.error = error;
+        this.loading = false;
+      },
+    });
   }
-  
 }
